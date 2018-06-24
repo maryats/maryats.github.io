c96c8b4ded4f3654fbe8b153f2a75ab43538fc0a
Marie Yatsyk <maryats@users.noreply.github.com>
2018-06-24 00:49:56 -0400

    Styling updates

--- js/draw-circle.js
+++ js/draw-circle.js
@@ -1,39 +1,89 @@
-let canvas = document.getElementById("canvas");
-canvas.height = window.innerHeight;
-canvas.width = window.innerWidth;
+(function () {
+  let canvas = document.getElementById("canvas"),
+    context = canvas.getContext("2d"),
+    isDrawing = true,
+    currentFunc = 0;
 
-let context = canvas.getContext("2d");
-context.translate(0.5, 0.5);
-
-let draw = true;
-
-const addRandomCircle = function (e) {
-  const radius = Math.random() * 40 + 1;
-  addCircle(e.clientX, e.clientY, radius);
-};
-
-const addCircle = function (x, y, r) {
-  context.beginPath();
-  context.arc(x, y, r, 0, 2 * Math.PI);
-  context.stroke();
-};
-
-const onMouseClick = function (e) {
-  draw = !draw;
-};
-
-const onMouseMove = function (e) {
-  if (draw) {
-    addRandomCircle(e);
+  function toggleIsDrawing() {
+    isDrawing = !isDrawing;
   }
-};
 
-const onKeyDown = function (e) {
-  if (e.keyCode === 32) {
+  function addRandomCircle(e) {
+    const radius = Math.random() * 40 + 1;
+    const colors = [
+      '#F262D3',
+      '#FF7D00',
+      '#FFD500',
+      '#3DD94A',
+      '#41C4FF',
+      '#9574EA'
+    ];
+
+    const idx =  Math.floor(Math.random() * colors.length);
+    context.strokeStyle = colors[idx];
+    addCircle(e.clientX, e.clientY, radius);
+  }
+
+  function addCircle(x, y, r) {
+    context.beginPath();
+    context.arc(x, y, r, 0, 2 * Math.PI);
+    context.stroke();
+  }
+
+  function clearCanvasOnSpace(e) {
+    if (e.keyCode === 32) {
+      clearCanvas();
+    }
+  }
+
+  // Draws bubbles wherever mouse moved
+  const drawBubbles = [
+    ['click', toggleIsDrawing, false],
+    ['mousemove', e => { if (isDrawing) { addRandomCircle(e)} }, false],
+    ['keydown', clearCanvasOnSpace, false]
+  ];
+
+  const functions = [drawBubbles];
+
+  function initialize() {
+    // make drawing less blurry
+    context.translate(0.5, 0.5);
+
+    // set initial canvas size
+    resizeCanvas();
+
+    setWindowEventListeners();
+    setCanvasEventListeners();
+  }
+
+  function setWindowEventListeners() {
+    window.addEventListener('resize', resizeCanvas, false);
+    window.addEventListener('keydown', switchFunctions, false);
+  }
+
+  function setCanvasEventListeners() {
+    functions[currentFunc].forEach((l) => {
+      canvas.addEventListener(l[0], l[1], l[2]);
+    })
+  }
+
+  function clearCanvas() {
     context.clearRect(0, 0, canvas.width, canvas.height);
   }
-};
 
-canvas.addEventListener('click', onMouseClick, false);
-canvas.addEventListener('mousemove', onMouseMove, false);
-canvas.addEventListener('keydown', onKeyDown, false);
+  function resizeCanvas() {
+    canvas.height = window.innerHeight;
+    canvas.width = window.innerWidth;
+  }
+
+  function switchFunctions(e) {
+    if (e.keyCode === 37) { // left
+      currentFunc = (currentFunc - 1) < 0 ? functions.length - 1 : currentFunc - 1;
+    }
+    else if (e.keyCode === 39) { // right
+      currentFunc = (currentFunc + 1) >= functions.length ? 0 : currentFunc + 1;
+    }
+  }
+
+  initialize();
+})();

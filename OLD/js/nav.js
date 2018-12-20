767e9e060b6201e88909cb89fd54f8e40f4aedbc
maryats <maryats@users.noreply.github.com>
2018-12-19 22:20:11 -0500

    Delete old files

--- OLD/js/nav.js
+++
@@ -1,6 +0,0 @@
-let $hamburger = $(".hamburger");
-let $navItems = $('.nav-items');
-$hamburger.on("click", function (e) {
-  $hamburger.toggleClass("is-active");
-  $navItems.toggleClass("nav-open");
-});
